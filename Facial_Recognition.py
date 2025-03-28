ac941952d4cf41c328ec59bde2ef002efffea850
Saksham <malhotsm@mail.uc.edu>
2025-03-27 20:06:15 -0400

    Branch created for Facial Recognition Feature

---
+++ Facial_Recognition.py
@@ -0,0 +1,10 @@
+import cv2
+
+cap = cv2.VideoCapture(0) 
+cap.set(3,640)
+cap.set(4,480)
+
+while True: 
+    success, img = cap.read()
+    cv2.imshow("Facial_Recognition", img)
+    cv2.waitKey(1)
